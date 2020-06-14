40f1ccc1fae25859632e2d02a49595f65cb43c5f
JB <jb@visualjerk.de>
2020-06-15 00:40:23 +0200

    Updates

---
+++ precache-manifest.3cb18c0e05743efc95d41ddaf7d9efb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276a7b8a219429905e4312d1ee338a22",
+    "url": "/ui-color-palette-generator/index.html"
+  },
+  {
+    "revision": "139999f8a130bc0030a1",
+    "url": "/ui-color-palette-generator/static/js/2.1524ab37.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/ui-color-palette-generator/static/js/2.1524ab37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c762d8827ae336a57d9",
+    "url": "/ui-color-palette-generator/static/js/main.c9621bd0.chunk.js"
+  },
+  {
+    "revision": "e7b69439e2231bd85dff",
+    "url": "/ui-color-palette-generator/static/js/runtime-main.18112e4d.js"
+  }
+]);
