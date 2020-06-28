3d6aacf1ee8c0da9ba6a8f27e285e9f40078e7cb
JB <jb@visualjerk.de>
2020-06-28 21:49:39 +0200

    feat: add shades

--- src/components/ColorTile.js
+++ src/components/ColorTile.js
@@ -1,8 +1,39 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import { Box, Text } from 'theme-ui'
+import chroma from 'chroma-js'
 
 function ColorTile(props) {
+  const base = chroma(props.color)
+  const baseL = base.get('hsl.l')
+
+  const lightVariantCount = Math.floor((1 - baseL) * 10)
+  const darkVariantCount = 10 - lightVariantCount
+
+  const variants = [props.color]
+
+  if (lightVariantCount > 0) {
+    const lightBase = base.set('hsl.l', 0.97)
+    const lightVariants = chroma
+      .scale([lightBase, base])
+      .mode('hsl')
+      .correctLightness()
+      .colors(lightVariantCount + 1)
+    lightVariants.pop()
+    variants.unshift(...lightVariants)
+  }
+
+  if (darkVariantCount > 0) {
+    const darkBase = base.set('hsl.l', 0.12)
+    const darkVariants = chroma
+      .scale([base, darkBase])
+      .mode('hsl')
+      .correctLightness()
+      .colors(darkVariantCount + 1)
+    darkVariants.shift()
+    variants.push(...darkVariants)
+  }
+
   return (
     <Box
       sx={{
@@ -25,7 +56,10 @@ function ColorTile(props) {
       <Text>
         <strong>{props.name}</strong>
       </Text>
-      <Text>{props.color}</Text>
+      <Text mb={2}>{props.color}</Text>
+      {variants.map((color, index) => (
+        <Box sx={{ backgroundColor: color, width: 7, height: 3 }}>{color}</Box>
+      ))}
     </Box>
   )
 }
